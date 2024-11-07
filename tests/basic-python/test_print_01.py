06b3f2c43a4402efae5170dc0bb3f4f15b860e50
amielpeled <amielnoy@gmail.com>
2024-11-07 22:01:56 +0200

    add  print

---
+++ tests/basic-python/test_print_01.py
@@ -0,0 +1,16 @@
+def test_loops():
+    print("\n\n")
+    print("Hello world!ֿ")
+    print("\n\n")
+
+    print("Well we")
+    print("just use more lines")
+    print("of code")
+    print("\n\n")
+
+    print("""Anything that starts
+    with three quotes, and ends
+    in three quotes can span
+    many lines and even contain " symbols
+    within it without freaking anything out!""")
+    print("\n\n")
