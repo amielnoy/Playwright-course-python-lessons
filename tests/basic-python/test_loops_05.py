1c3b541744c3d7b5c0aa414fe79f8969701abf89
amielpeled <amielnoy@gmail.com>
2024-11-07 22:01:38 +0200

    add loops & print

---
+++ tests/basic-python/test_loops_05.py
@@ -0,0 +1,8 @@
+def test_loops():
+        # simple loop
+        for i in range(0,4):
+            print('iteration==' +str(i));
+
+        # step step=2
+        for i in range(0,2,8):
+            print('iteration==' +str(i));
